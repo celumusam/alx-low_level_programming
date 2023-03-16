bb2782f6ca8a4d84ad9970ee4bf5de9e4efafc12
celumusam <celmusacydarmlotsa@gmail.com>
2023-03-16 02:15:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * main - A program that print a line with puts function
  * Return: 0 (Success)
  */
-int main(void) 
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
 
