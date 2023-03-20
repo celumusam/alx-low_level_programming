b70dd894aa8d4f3676c7243834e767b639456642
celumusam <celmusacydarmlotsa@gmail.com>
2023-03-20 04:06:56 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 /**
- * main –main function.
+ * main – main function
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 int i;
-char j;
 
 for (i = 0 ; i < 10 ; i++)
+{
+	if (i == 9)
+		putchar(i + '0');
+	else
 putchar(i + '0');
-for (j = 'a' ; j <= 'f' ; j++)
-putchar(j);
-putchar('\n');
+putchar(',');
+putchar(' ');
+}
 return (0);
 }
