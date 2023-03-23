e5d16fe688e53b4bd58fce0dd7a5994c79327a0c
celumusam <celmusacydarmlotsa@gmail.com>
2023-03-23 05:50:05 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,22 +4,25 @@
 /**
  * print_number - Print an integer.
  * @n: The integer to prints.
- * Return: Nothing
  */
 
 void print_number(int n)
 {
 
-unsigned int k = n;
+unsigned int n1;
 
 if (n < 0)
 {
-n *= 1;
-k = n;
+n1 = -n;
 _putchar('_');
 }
-k /= 10;
-if (k != 0)
-print_number(k);
-_putchar((unsigned int) n % 10 + '0');
+else
+{
+n1 = n;
+}
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
