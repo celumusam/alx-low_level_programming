681b64a4b09fe03b74c2561b7b3554aaf72e3234
celumusam <celmusacydarmlotsa@gmail.com>
2023-03-23 03:37:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,17 +9,12 @@
 void print_most_numbers(void)
 {
 
-int x = 0;
+char c;
 
-for (; x <= 9; x++)
+for (c = '0'; c <= '9'; c++)
 {
-if (x == 2 || x == 4)
-{
-continue;
+if (!(c == '2' || c == '4'))
+_putchar(c);
 }
-else
-_putchar(x + '0');
-}
-}
-_putchar('\n')
+_putchar('\n');
 }
