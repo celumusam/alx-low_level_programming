f303dd4f06c82a015f9d09d5fc13e687bac6916b
celumusam <celmusacydarmlotsa@gmail.com>
2023-03-29 02:22:24 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ int j;
 i = 0;
 while (dest[i] != '\0')
 {
-i++
+i++;
 }
 j = 0;
 while (j < n && src[j] != '\0')
@@ -27,6 +27,6 @@ dest[i] = src[j];
 i++;
 j++;
 }
-desr[i] = '\0';
+dest[i] = '\0';
 return (dest);
 }
