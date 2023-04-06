e8a5d995f3bd5a1b6202ecd4915847b521c9f85a
celumusam <celmusacydarmlotsa@gmail.com>
2023-04-06 02:36:19 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -37,7 +37,7 @@ return (1 + _strlen_recursion(s + 1));
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_pal(char *s, in i, int len)
+int check_pal(char *s, int i, int len)
 {
 if (*(s + i) != *(s + len - 1))
 return (0);
