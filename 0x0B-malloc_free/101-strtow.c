ab3abb1f65ba0e748528d9ed814aa0321696c42c
celumusam <celmusacydarmlotsa@gmail.com>
2023-04-11 04:40:56 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -76,7 +76,7 @@ while (str[index] == ' ')
 index++;
 
 letters = word_len(str + index);
-strings[w] malloc(sizeof(char) * (letters + 1));
+strings[w] =  malloc(sizeof(char) * (letters + 1));
 if (strings[w] == NULL)
 {
 for (; w >= 0; w--)
