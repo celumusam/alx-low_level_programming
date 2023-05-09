1bf17b3c2460f4e78cce19aeb2183774854ac9cd
celumusam <celmusacydarmlotsa@gmail.com>
2023-05-09 03:08:40 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ for (len = 0; text_content[len];)
 len++;
 }
 
-fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 w = write(fd, text_content, len);
 
 if (fd == -1 || w == -1)
